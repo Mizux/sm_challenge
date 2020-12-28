f4d5fa28460bbd69157c5d237afb4c95d5553bbc
Mizux Seiha <mizux.dev@gmail.com>
2020-12-28 12:00:56 +0100

    app: Add any Point on M feature

--- App/src/Path.cpp
+++ App/src/Path.cpp
@@ -40,16 +40,15 @@ std::vector<std::vector<int>> compute_adjacency_list(
 
 Eigen::VectorXd get_point(const int index, const Eigen::MatrixXd& vertices) {
   assert(index < vertices.rows());
-  Eigen::VectorXd result(vertices.cols());
+  Eigen::VectorXd vector(vertices.cols());
   for (int i = 0; i < vertices.cols(); ++i) {
-    result(i) = vertices(index, i);
+    vector(i) = vertices(index, i);
   }
-  return result;
+  return vector;
 }
 
-double length(const int start, const int end, const Eigen::MatrixXd& vertices,
-              const std::function<double(const Eigen::VectorXd&,
-                                         const Eigen::VectorXd)>& cost) {
+double edge_length(const int start, const int end,
+                   const Eigen::MatrixXd& vertices, const CostCallback& cost) {
   assert(start < vertices.rows());
   assert(end < vertices.rows());
   if (start == end) return double{0};
@@ -60,12 +59,97 @@ double length(const int start, const int end, const Eigen::MatrixXd& vertices,
   return cost(s, e);
 }
 
+std::tuple<Distances, Parents> shortest_path(const std::vector<int>& sources,
+                                             const int target,
+                                             const Eigen::MatrixXd& vertices,
+                                             const Eigen::MatrixXi& faces,
+                                             const CostCallback& cost,
+                                             bool compute_all_distances) {
+  std::vector<std::tuple<int, double>> src_dst;
+  src_dst.reserve(sources.size());
+
+  for (const auto it : sources) {
+    assert(it < vertices.rows());
+    src_dst.emplace_back(it, 0.);
+  }
+  assert(target < vertices.rows());
+
+  return shortest_path(src_dst, target, vertices, faces, cost,
+                       compute_all_distances);
+}
+
+struct ClosestNode {
+  int index;
+  double distance;
+};
+
+// Find the closest vertex of M for each point in the list and return its index
+// and distance.
+std::vector<std::tuple<int, double>> closest_vertices(
+    const std::vector<Eigen::VectorXd>& pts, const Eigen::MatrixXd& vertices) {
+  auto square_distance = [&](const Eigen::VectorXd& s,
+                             const Eigen::VectorXd& e) -> double {
+    assert(s.rows() == e.rows());
+    assert(s.cols() == e.cols());
+    double sum = double{0};
+    for (int i = 0; i < s.rows(); ++i) {
+      sum += std::pow(e(i) - s(i), double{2.0});
+    }
+    // sqrt is not needed to find the closest point
+    return sum;
+  };
+
+  std::vector<std::tuple<int, double>> closest_nodes(
+      pts.size(), {-1, std::numeric_limits<double>::max()});
+  for (int vertex_id = 0; vertex_id < vertices.rows(); ++vertex_id) {
+    auto vertex = get_point(vertex_id, vertices);
+    for (std::size_t i = 0; i < pts.size(); ++i) {
+      double distance = square_distance(pts[i], vertex);
+      if (distance < std::get<1>(closest_nodes[i])) {
+        std::get<0>(closest_nodes[i]) = vertex_id;
+        std::get<1>(closest_nodes[i]) = distance;
+      }
+    }
+  }
+
+  // Only compute std::sqrt for final points
+  for (auto& it : closest_nodes) {
+    std::get<1>(it) = std::sqrt(std::get<1>(it));
+  }
+  return closest_nodes;
+}
+
 std::tuple<Distances, Parents> shortest_path(
-    const std::vector<int>& sources, const int target,
+    const std::vector<Eigen::VectorXd>& sources, const Eigen::VectorXd& target,
     const Eigen::MatrixXd& vertices, const Eigen::MatrixXi& faces,
-    const std::function<double(const Eigen::VectorXd&, const Eigen::VectorXd)>&
-        cost) {
-  assert(source < vertices.rows());
+    const CostCallback& cost, bool compute_all_distances) {
+  const auto source_vertices = closest_vertices(sources, vertices);
+  const auto target_vertex = closest_vertices({target}, vertices).front();
+
+  return shortest_path(source_vertices, std::get<0>(target_vertex), vertices,
+                       faces, cost, compute_all_distances);
+}
+
+double geodesic_distance(const std::vector<Eigen::VectorXd>& sources,
+                         const Eigen::VectorXd& target,
+                         const Eigen::MatrixXd& vertices,
+                         const Eigen::MatrixXi& faces,
+                         const CostCallback& cost) {
+  // Get distance from source point to the target closest vertex of M.
+  const auto distances = shortest_path(sources, target, vertices, faces, cost,
+                                       /*compute_all_distances=*/false);
+
+  // Get distance from target to closest vertex of M using euclidean distance.
+  const auto target_vertex = closest_vertices({target}, vertices).front();
+
+  return std::get<0>(distances)[std::get<0>(target_vertex)] +
+         std::get<1>(target_vertex);
+}
+
+std::tuple<Distances, Parents> shortest_path(
+    const std::vector<std::tuple<int, double>>& sources, const int target,
+    const Eigen::MatrixXd& vertices, const Eigen::MatrixXi& faces,
+    const CostCallback& cost, bool compute_all_distances) {
   assert(target < vertices.rows());
 
   // Dijkstra using a priority queue.
@@ -74,14 +158,23 @@ std::tuple<Distances, Parents> shortest_path(
   std::vector<double> distances(num_nodes, std::numeric_limits<double>::max());
   std::vector<int> parents(num_nodes, std::numeric_limits<int>::quiet_NaN());
 
-  for (const auto src : sources) {
-    distances[src] = double{0};
-    parents[src] = src;
+  for (const auto& [index, dst] : sources) {
+    assert(index < vertices.rows());
+    // we may have duplicate sources index, i.e. same vertex but with different
+    // distance.
+    distances[index] = std::min(distances[index], dst);
+    parents[index] = index;
   }
-  // Target is a source point
-  // must be disable for question 2.5
-  if (std::find(sources.begin(), sources.end(), target) != sources.end())
-    return {distances, parents};
+
+  // Check if Target is a source point and we don't want to compute all
+  // distances.
+  if (!compute_all_distances) {
+    auto comp = [=](const std::tuple<int, double>& a) -> bool {
+      return std::get<0>(a) == target;
+    };
+    if (std::find_if(sources.begin(), sources.end(), comp) != sources.end())
+      return {distances, parents};
+  }
 
   // Create adjacency list
   auto adjacency_list = compute_adjacency_list(vertices, faces);
@@ -96,135 +189,31 @@ std::tuple<Distances, Parents> shortest_path(
   std::priority_queue<QueueNode, std::vector<QueueNode>, decltype(comp)> queue(
       comp);
 
-  for (const auto src : sources) {
-    queue.push({src, double{0}});
+  for (const auto& [index, dst] : sources) {
+    queue.push({index, dst});
   }
   while (!queue.empty()) {
-    QueueNode node = queue.top();
+    const auto [index, distance] = queue.top();
     queue.pop();
     // check if we need to update neighbours.
-    if (node.distance <= distances[node.index]) {
-      for (const auto& i : adjacency_list[node.index]) {
-        auto distance =
-            distances[node.index] + length(node.index, i, vertices, cost);
-        if (distance < distances[i]) {
-          distances[i] = distance;
-          parents[i] = node.index;
-          queue.push({i, distance});
+    if (distance <= distances[index]) {
+      for (const auto& adj_index : adjacency_list[index]) {
+        auto adj_distance =
+            distances[index] + edge_length(index, adj_index, vertices, cost);
+        if (adj_distance < distances[adj_index]) {
+          distances[adj_index] = adj_distance;
+          parents[adj_index] = index;
+          queue.push({adj_index, adj_distance});
         }
       }
     }
-
-    // uncomment to stop algorithm once target point is reach
-    // but you won't have distance, path for all vertices of M.
-    // if (node.index == target) {
-    //  return {distances, parents};
-    //}
+    // Stop algorithm once target point is fetch
+    // but you won't have all [distance, parent] for all vertices of M.
+    if ((!compute_all_distances) && (index == target)) {
+      return {distances, parents};
+    }
   }
   return {distances, parents};
-}
-
-struct ClosestNode {
-  int index;
-  double distance;
-};
-
-// Find the closest vertex of M from list of point pts.
-std::vector<ClosestNode> closest_nodes(const std::vector<Eigen::VectorXd>& pts,
-                                       const Eigen::MatrixXd& vertices) {
-  auto square_distance = [&](const Eigen::VectorXd& s,
-                             const Eigen::VectorXd& e) -> double {
-    assert(s.rows() == e.rows());
-    assert(s.cols() == e.cols());
-    double sum = double{0};
-    for (int i = 0; i < s.rows(); ++i) {
-      sum += std::pow(e(i) - s(i), double{2.0});
-    }
-    // don't compute the std::sqrt(sum)
-    return sum;
-  };
-
-  std::vector<ClosestNode> closest_nodes(
-      pts.size(), {-1, std::numeric_limits<double>::max()});
-  for (int vertex_id = 0; vertex_id < vertices.rows(); ++vertex_id) {
-    auto vertex = get_point(vertex_id, vertices);
-    for (std::size_t i = 0; i < pts.size(); ++i) {
-      double distance = square_distance(pts[i], vertex);
-      if (distance < closest_nodes[i].distance) {
-        closest_nodes[i].index = vertex_id;
-        closest_nodes[i].distance = distance;
-      }
-    }
-  }
-  return closest_nodes;
-}
-
-std::tuple<Distances, Parents> shortest_path(
-    const std::vector<Eigen::VectorXd>& sources, const Eigen::VectorXd& target,
-    const Eigen::MatrixXd& vertices, const Eigen::MatrixXi& faces,
-    const std::function<double(const Eigen::VectorXd&, const Eigen::VectorXd)>&
-        cost) {
-  auto source_vertices = closest_nodes(sources, vertices);
-  ClosestNode target_vertex = closest_nodes({target}, vertices).front();
-
-  // Dijkstra using a priority queue.
-  // ref: https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
-  auto num_nodes = vertices.rows();
-  std::vector<double> distances(num_nodes, std::numeric_limits<double>::max());
-  std::vector<int> parents(num_nodes, std::numeric_limits<int>::quiet_NaN());
-
-  // This time, initialize using the closest vertices
-  std::set<int> start_indices;
-  for (const auto& it : source_vertices) {
-    // if two source points map to the same vertex only keep the closest one.
-    distances[it.index] = std::min(distances[it.index], it.distance);
-    parents[it.index] = it.index;
-    start_indices.insert(it.index);
-  }
-
-  // Create the vertices adjacency list
-  auto adjacency_list = compute_adjacency_list(vertices, faces);
-  // Create the priority queue
-  struct QueueNode {
-    int index;
-    double distance;
-  };
-  auto comp = [](const QueueNode& a, const QueueNode& b) {
-    return a.distance > b.distance;
-  };
-  std::priority_queue<QueueNode, std::vector<QueueNode>, decltype(comp)> queue(
-      comp);
-
-  // Init the priority queue by adding the closest vertices.
-  for (const auto index : start_indices) {
-    queue.push({index, distances[index]});
-  }
-
-  while (!queue.empty()) {
-    QueueNode node = queue.top();
-    queue.pop();
-    // check if we need to update neighbours.
-    if (node.distance <= distances[node.index]) {
-      for (const auto& i : adjacency_list[node.index]) {
-        auto distance =
-            distances[node.index] + length(node.index, i, vertices, cost);
-        if (distance < distances[i]) {
-          distances[i] = distance;
-          parents[i] = node.index;
-          queue.push({i, distance});
-        }
-      }
-    }
-
-    // to stop once we reach the closest vertex from the target.
-    // if (node.index == target_vertex.index) {
-    //  return {distances, parents};
-    //}
-  }
-
-  return {distances, parents};
-  // geodesic shortest distance is: "distances[target_vertex.index] +
-  // target_vertex.distance"
 }
 
 }  // namespace sm
--- App/src/Path.hpp
+++ App/src/Path.hpp
@@ -9,25 +9,38 @@ namespace sm {
 
 using Distances = std::vector<double>;
 using Parents = std::vector<int>;
+using CostCallback =
+    std::function<double(const Eigen::VectorXd&, const Eigen::VectorXd&)>;
 
 std::vector<std::vector<int>> compute_adjacency_list(
     const Eigen::MatrixXd& vertices, const Eigen::MatrixXi& faces);
 
 Eigen::VectorXd get_point(const int index, const Eigen::MatrixXd& vertices);
 
-double distance(const int start, const int end,
-                const Eigen::MatrixXd& vertices);
+// Sources and Target are vertices of the Mesh.
+std::tuple<Distances, Parents> shortest_path(const std::vector<int>& sources,
+                                             const int target,
+                                             const Eigen::MatrixXd& vertices,
+                                             const Eigen::MatrixXi& faces,
+                                             const CostCallback& cost,
+                                             bool compute_all_distances = true);
 
+// Sources and Target are 3D point on the Mesh surface.
 std::tuple<Distances, Parents> shortest_path(
-    const std::vector<int>& sources, const int target,
+    const std::vector<Eigen::VectorXd>& sources, const Eigen::VectorXd& target,
     const Eigen::MatrixXd& vertices, const Eigen::MatrixXi& faces,
-    const std::function<double(const Eigen::VectorXd&, const Eigen::VectorXd)>&
-        cost);
+    const CostCallback& cost, bool compute_all_distances = true);
 
+double geodesic_distance(const std::vector<Eigen::VectorXd>& sources,
+                         const Eigen::VectorXd& target,
+                         const Eigen::MatrixXd& vertices,
+                         const Eigen::MatrixXi& faces,
+                         const CostCallback& cost);
+
+// Associate a default distance to Sources vertices.
 std::tuple<Distances, Parents> shortest_path(
-    const std::vector<Eigen::VectorXd>& sources, const int target,
+    const std::vector<std::tuple<int, double>>& sources, const int target,
     const Eigen::MatrixXd& vertices, const Eigen::MatrixXi& faces,
-    const std::function<double(const Eigen::VectorXd&, const Eigen::VectorXd)>&
-        cost);
+    const CostCallback& cost, bool compute_all_distances = true);
 
 }  // namespace sm
